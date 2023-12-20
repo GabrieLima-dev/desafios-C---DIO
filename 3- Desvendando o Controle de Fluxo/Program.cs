380c2712d7e0aa2110116132709a68baf0c4606a
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-12-20 16:01:52 -0300

    Desafio Desvendando o Controle de Fluxo

--- 3- Desvendando o Controle de Fluxo/Program.cs
+++ 3- Desvendando o Controle de Fluxo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    // Solicita ao usuário os limites inferiores e superiores
+    Console.WriteLine("");
+    int limiteInferior = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    int limiteSuperior = int.Parse(Console.ReadLine());
+
+    // Variável para acumular a soma dos números pares
+    int somaPares = 0;
+
+    // TODO: Crie um Loop para percorrer os números no intervalo
+    for (int i = limiteInferior; i <= limiteSuperior; i++){
+        if (i % 2 == 0){
+            somaPares = i + somaPares;
+        }
+        
+    }      
+
+    // Exibe o resultado
+    Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+    }
+}
