c63f4a65946458a88e037b63cc838ccd11e8ed92
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-12-26 12:00:11 -0300

    Utilizando Propriedades e Métodos

---
+++ 7- Utilizando Propriedades e Métodos/Program.cs
@@ -0,0 +1,26 @@
+﻿using _7__Utilizando_Propriedades_e_Métodos.Services;
+
+class Program
+{
+    static void Main()
+    {
+        string[] valores = Console.ReadLine().Split(' '); int vmin = int.Parse(valores[0]); int vmax = int.Parse(valores[1]);
+        string comandos = Console.ReadLine();
+
+        Robo robo = new Robo(vmin, vmax);
+
+        foreach (var comando in comandos)
+        {
+            switch (comando)
+            {
+                case 'A' or 'a':
+                    robo.Acelerar();
+                    break;
+                case 'D' or 'd':
+                    robo.Desacelerar();
+                    break;
+            }
+        }
+        Console.WriteLine(robo.VelocidadeAtual);
+    }
+}
