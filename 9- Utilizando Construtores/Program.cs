f737bc6116975a0b04c7070bbf8d3559bed37f3c
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-12-26 13:51:11 -0300

    Utilizando Construtores

---
+++ 9- Utilizando Construtores/Program.cs
@@ -0,0 +1,21 @@
+﻿using _9__Utilizando_Construtores.Services;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("");
+        string raca = Console.ReadLine();
+
+        Console.WriteLine("");
+        string classe = Console.ReadLine();
+
+        Personagem personagem = new Personagem(nome, raca, classe);
+
+        Console.WriteLine("Status:");
+        personagem.ExibirStatus();
+    }
+}
---
+++ 9- Utilizando Construtores/Services/Personagem.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _9__Utilizando_Construtores.Services
+{
+    class Personagem
+    {
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public string Classe { get; set; }
+        public int Nivel { get; set; } = 1;
+        public int Vida { get; set; } = 10;
+
+        public Personagem(string nome, string raca, string classe)
+        {
+            Nome = nome;
+            Raca = raca;
+            Classe = classe;
+        }
+
+        public void ExibirStatus()
+        {
+            Console.WriteLine("Nome:" + Nome);
+            Console.WriteLine("Raça:" + Raca);
+            Console.WriteLine("Classe:" + Classe);
+            Console.WriteLine("Nível:" + Nivel);
+            Console.WriteLine("Vida:" + Vida);
+        }
+    }
+}
